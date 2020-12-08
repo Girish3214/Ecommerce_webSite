4b452cbf4d99ae5e51250869c7c02994a2f07248
Girish <girishmroy143@gmail.com>
2020-12-08 16:12:02 +0530

    controller changes

--- back_end/server.js
+++ back_end/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 import dotenv from "dotenv"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
@@ -11,12 +12,15 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 
 app.get("/", (req,res)=>{
     res.send("Api is started")
 })
 
 app.use("/api/products", productRoutes)
+app.use("/api/users", userRoutes)
 
 app.use(notFound)
 
